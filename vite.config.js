0cfee7a7ab8ea8fa246f2a564b441e9b6e81054d
aabdulhu <ali.almoumnin@gmail.com>
2024-11-20 15:45:41 +0300

    P30

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,3 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  base: '/graphql/', // The repository name as the base path
-  build: {
-    rollupOptions: {
-      input: '/index.html', // Ensure Rollup resolves the correct entry point
-    },
-  },
-});
+export default {
+  base: '/graphql/',
+};

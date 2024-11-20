6487e0fff13a44aba0e7279e5a726d0360dd916b
aabdulhu <ali.almoumnin@gmail.com>
2024-11-20 17:55:22 +0300

    P30

--- src/app/router.ts
+++ src/app/router.ts
@@ -2,15 +2,16 @@ import { createHomePage } from "./homePage";
 import { renderLoginPage } from "./loginPage";
 import { renderProfilePage } from "./profilePage";
 import { renderNotFoundPage } from "./notFoundPage";
+const base = location.href;
 
 // Function to render the page based on the current path
 export function renderPage() {
     const path = location.pathname;
-    if (path === "/profile") {
+    if ((base + path) === (base + "/profile")) {
         renderProfilePage();
-    } else if (path === "/login") {
+    } else if ((base + path) === (base + "/login")) {
         renderLoginPage();
-    } else if (path === "/") {
+    } else if ((base + path) === (base + "/")) {
         createHomePage();
     } else {
         renderNotFoundPage();
